56000827277d97483c13c29e9cc5b0414a622205
julkaaaaaaa <julcia.lorenz@gmail.com>
2024-05-25 17:09:44 +0200

    Add visualizations to dashboard, modified dashboard appearance

--- eurovision_stats/server.R
+++ eurovision_stats/server.R
@@ -7,10 +7,22 @@ library(plotly)
 library(ggiraph)
 library(ggplot2)
 library(packcircles)
+library(tidyverse)
+library(highcharter)
 
 
 function(input, output, session) {
-  # the map visualization
+# ------------------------------------------------------------------------------------------------------------
+  output$contests <- renderValueBox({
+    events <- read.csv("datasets/events_info.csv")
+    valueBox(
+      color = "fuchsia",
+      value = paste(nrow(events), 'CONTESTS'),
+      subtitle = "ESC has been held annualy for almost 70 years!"
+      )
+  })  
+
+# ------------------------------------------------------------------------------------------------------------
   output$map <- renderGirafe({
     results = read.csv("datasets/eurovision_results.csv")
     events = read.csv("datasets/events_info.csv")
@@ -26,7 +38,7 @@ function(input, output, session) {
     world$events[is.na(world$events)] <- 0
     world$events <- as.factor(world$events)
     
-    Europe <- world[which(world$continent == "Europe"), ]
+    Europe <- world[which(world$continent == "Europe"),]
     custom_colors = rev(
       c(
         "#6B10C5",
@@ -43,7 +55,8 @@ function(input, output, session) {
       geom_sf_interactive(aes(
         fill = events,
         tooltip = paste('Country: ', name, '\n',
-                        'Number of events hosted: ', events)),
+                        'Number of events hosted: ', events)
+      ),
       color = 'white',
       size = 0.6) +
       theme_void() +
@@ -52,13 +65,24 @@ function(input, output, session) {
                ylim = c(35, 70),
                expand = FALSE) +
       labs(fill = "Number of events") +
-      theme(legend.position = "null",
-            panel.background = element_rect(fill = "#010039"),
-            plot.background = element_rect(fill = "#010039"),
-            plot.title = element_text(color = "white", size = 20, hjust = 0.5))
-    girafe(ggobj = p, width_svg = 10, height_svg = 8)
+      theme(
+        legend.position = "none",
+        panel.background = element_rect(fill = "#010039"),
+        plot.background = element_rect(fill = "#010039"),
+        plot.title = element_text(
+          color = "white",
+          size = 20,
+          hjust = 0.5
+        ),
+        plot.margin = unit(c(0, 0, 0, 0), 'cm')
+      )
+    girafe(ggobj = p,
+           width_svg = 2*8.9,
+           height_svg = 2*6.8436,
+           options = list(opts_sizing(rescale = T)))
   })
-  
+
+# ------------------------------------------------------------------------------------------------------------
   # the language visualization
   output$circles <- renderGirafe({
     results = read.csv("datasets/eurovision_results.csv")
@@ -72,16 +96,163 @@ function(input, output, session) {
     lang_count$Count = lang_freq$Freq
     colnames(lang_count) = c("Language", "Proportion", "Count")
     lang_count$Proportion = round(lang_count$Proportion * 100, 2)
-    lang_count = lang_count[lang_count$Proportion > 3.0,]
+    lang_count = lang_count[lang_count$Proportion > 3.0, ]
     
-    # prepare the circles 
-    packing <- circleProgressiveLayout(lang_count$Proportion, sizetype='area')
+    # prepare the circles
+    packing <-
+      circleProgressiveLayout(lang_count$Proportion, sizetype = 'area')
     data <- cbind(lang_count, packing)
-    data$text <- paste("Language: ",data$Language, "\n", "Proportion: ", data$Proportion, "\n", 
-                       "Proportion of ", data$Language, 
-                       " eurovision songs is ", data$Proportion, 
-                       ", that is ", data$Count, " songs.")
-    dat.gg <- circleLayoutVertices(packing, npoints=50)
+    data$text <-
+      paste(
+        "Language: ",
+        data$Language,
+        "\n",
+        "Proportion: ",
+        data$Proportion,
+        "\n",
+        "Proportion of ",
+        data$Language,
+        " eurovision songs is ",
+        data$Proportion,
+        ", that is ",
+        data$Count,
+        " songs."
+      )
+    dat.gg <- circleLayoutVertices(packing, npoints = 50)
+    
+    custom_colors <- c(
+      "#6B10C5",
+      "#aa20ab",
+      "#d9009b",
+      "#ff1e6e",
+      "#ff5f49",
+      "#FF7814",
+      "#ff9e35",
+      "#ffa65b",
+      "#ffd254",
+      "#f9f871"
+    )
+    dat.gg$id <- as.factor(dat.gg$id)
+    p <- ggplot() +
+      geom_polygon_interactive(data = dat.gg, aes(
+        x,
+        y,
+        group = id,
+        fill = id,
+        tooltip = data$text[id]
+      )) +
+      geom_text(
+        data = data,
+        aes(x, y, size = Proportion, label = Language),
+        color = "white"
+        ) +
+      scale_fill_manual(values = custom_colors) +
+      scale_size_continuous(range = c(3, 7)) +
+      theme_void() +
+      coord_equal() +
+      theme(
+        legend.position = "none",
+        panel.background = element_rect(fill = "#010039"),
+        plot.background = element_rect(fill = "#010039"),
+        plot.margin = unit(c(0, 0, 0, 0), 'cm')
+      )
+    
+    widg <- girafe(ggobj = p,
+                   width_svg = 0.7*5.81, height_svg = 0.7*4,
+                   options = list(opts_sizing(rescale = F)))
+  })
+  
+# ------------------------------------------------------------------------------------------------------------
+  
+  output$winners <- renderHighchart({
+    data <- read.csv("datasets/eurovision_results.csv")
+    data_finals <- data %>%
+      filter(Grand.Final.Place == 1) %>%
+      group_by(Country) %>%
+      summarise(Count = n(),
+                Wins = list(paste(
+                  Year, paste0('"', Song, '" by ', Artist), sep = " - "
+                ))) %>%
+      arrange(desc(Count))
+    
+    # Combine the wins into a single string
+    data_finals <- data_finals %>%
+      mutate(Wins = sapply(Wins, paste, collapse = "<br>"))
+    
+    custom_colors <- c("#6B10C5",
+                       "#aa20ab",
+                       "#d9009b",
+                       "#ff1e6e",
+                       "#ff5f49",
+                       "#FF7814",
+                       "#ff9e35")
+    
+    # Create the treemap chart with customized tooltip
+    hc <- data_finals %>%
+      hchart(
+        "treemap",
+        hcaes(
+          x = Country,
+          value = Count,
+          color = Count
+        ),
+        tooltip = list(
+          pointFormat = '<b>{point.name}</b><br>
+                     Count: {point.value}<br>
+                     Wins:<br>{point.Wins}',
+          style = list(width = '200px')
+        )
+      ) %>%
+      hc_colorAxis(stops = color_stops(colors = rev(custom_colors))) %>%
+      hc_legend(enabled = FALSE)
+    
+    # Customize the tooltip
+    hc <- hc %>%
+      hc_tooltip(
+        useHTML = TRUE,
+        headerFormat = '',
+        pointFormat = '<b>{point.name}</b><br>
+                   Count: {point.value}<br>
+                   Wins:<br>{point.Wins}'
+      )
+    
+    custom_theme <- hc_theme(
+      chart = list(backgroundColor = "#010039"),
+      title = list(
+        style = list(
+          color = "white",
+          fontFamily = "Cantarell",
+          fontSize = "20px"
+        )
+      ),
+      tooltip = list(
+        backgroundColor = "#000000",
+        style = list(
+          color = "#ffffff",
+          fontFamily = "Cantarell",
+          fontSize = "12px"
+        )
+      )
+    )
+    # Apply the custom theme
+    hc <- hc %>%
+      hc_add_theme(custom_theme)
+  })
+  
+# ------------------------------------------------------------------------------------------------------------
+  output$points <- renderGirafe({
+    data=read.csv("datasets/eurovision_results.csv")
+    years = strsplit(input$select, "-")
+    year1 = as.numeric(years[[1]][1])
+    year2 = as.numeric(years[[1]][2])
+    most_points <- data %>%
+      filter(
+        Year >= year1 & Year <= year2,
+        !is.na(as.numeric(Grand.Final.Points))
+      )%>%
+      mutate(Grand.Final.Points = as.integer(Grand.Final.Points)) %>%
+      arrange(desc(Grand.Final.Points)) %>%
+      head(10)
     
     custom_colors <- c(
       "#6B10C5",
@@ -95,20 +266,28 @@ function(input, output, session) {
       "#ffd254",
       "#f9f871"
     )
-    dat.gg$id <- as.factor(dat.gg$id)
-    p <- ggplot() + 
-      geom_polygon_interactive(data = dat.gg, aes(x, y, group = id, fill=id, tooltip = data$text[id])) +
-      geom_text(data = data, aes(x, y, size=Proportion, label = Language), color = "white") +
-      scale_fill_manual(values = custom_colors) +
-      scale_size_continuous(range = c(3,7)) +
-      theme_void() + 
-      coord_equal() +
-      theme(legend.position = "null",
-            panel.background = element_rect(fill = "#010039"),
-            plot.background = element_rect(fill = "#010039"),
-            plot.title = element_text(color = "white", size = 20, hjust = 0.5))
     
-    widg <- girafe(ggobj = p, width_svg = 10, height_svg = 8)
+    g <- ggplot(most_points, aes(x = reorder(Song, Grand.Final.Points), y = Grand.Final.Points, fill = reorder(Song, - Grand.Final.Points))) +
+      geom_col_interactive(aes(tooltip = paste0(Country, "<br> \"", Song,"\" by ", Artist, "<br>Year: ", Year, "<br>Points: ", Grand.Final.Points))) +
+      geom_text(aes(x = reorder(Song, Grand.Final.Points), y = 0, label = paste0("\"",Song,"\"")), 
+                color = "white", size = 5, fontface = "bold", vjust = 0.5, hjust = 0) +
+      scale_fill_manual(values = custom_colors) +
+      labs(title = paste("Most points received ", year1, " - ", year2)) +
+      coord_flip() +
+      theme(
+        legend.position = "none",
+        panel.background = element_rect(fill = "#010039"),
+        plot.background = element_rect(fill = "#010039"),
+        axis.text.y = element_blank(),  
+        axis.title.y = element_blank(), 
+        plot.title = element_text(color = "white", size = 20, hjust = 0.5),
+        axis.text = element_text(color = "white", size = 14, face = "bold"),
+        panel.grid.major.y = element_blank(), 
+        panel.grid.minor.y = element_blank(),
+        panel.grid.minor = element_blank() 
+      )
+    
+    widg <- girafe(ggobj = g, width_svg = 10, height_svg = 10)
   })
   
 }
--- eurovision_stats/ui.R
+++ eurovision_stats/ui.R
@@ -9,36 +9,72 @@ library(ggiraph)
 library(shinydashboard)
 
 dashboardPage(
-  dashboardHeader(title = div(
-    style = "display: flex; align-items: baseline; justify-content: center; margin-top: 2px;",
-    div("EUROVISION", style = "color: #FFF400; font-size: 14px; font-weight: 700; font-family: 'Cantarell', sans-serif; margin-right: 10px;"),
-    div("STATISTICS", style = "color: white; font-size: 14px; font-weight: 400; font-family: 'Cantarell', sans-serif;")
-  )),
+  dashboardHeader(
+    title = div(
+      style = "display: flex; align-items: baseline; justify-content: center; margin-top: 2px;",
+      div("EUROVISION", style = "color: #FFF400; font-size: 14px; font-weight: 700; font-family: 'Cantarell', sans-serif; margin-right: 10px;"),
+      div("STATISTICS", style = "color: white; font-size: 14px; font-weight: 400; font-family: 'Cantarell', sans-serif;")
+    )
+  ),
   dashboardSidebar(
-    # main page
+    includeCSS("www/style.css"),
+    menuItem("Main page", tabName = "main"),
+    menuItem("Other page", tabName = "other")
     # maybe some other pages
   ),
-  dashboardBody(
-    includeCSS("www/style.css"),
-    fluidRow(column(
-      width = 10,
-      div(
-        style = "display: flex; align-items: baseline; justify-content: left; margin-top: 1px;",
-        div("EUROVISION", style = "color: #FFF400; font-size: 30px; font-weight: 700; font-family: 'Cantarell', sans-serif; margin-right: 10px;"),
-        div("STATISTICS", style = "color: white; font-size: 30px; font-weight: 400; font-family: 'Cantarell', sans-serif;")
-      )
-    )),
-    fluidRow(splitLayout(
-      box(
-        title = "The Most Frequent Eurovision Hosts", 
+  dashboardBody(tabItems(
+    tabItem(
+      tabName = "main",
+      includeCSS("www/style.css"),
+      fluidRow(column(
         width = 10,
-        girafeOutput("map")
-      ),
-      box(
-        title = "The Most Common Language In Eurovision Songs", 
+        div(
+          style = "display: flex; align-items: baseline; justify-content: left; margin-top: 1px;",
+          div("EUROVISION", style = "color: #FFF400; font-size: 30px; font-weight: 700; font-family: 'Cantarell', sans-serif; margin-right: 10px;"),
+          div("STATISTICS", style = "color: white; font-size: 30px; font-weight: 400; font-family: 'Cantarell', sans-serif;")
+        )
+      )),
+      fluidRow(splitLayout(
+        box(title = "The Most Frequent Eurovision Hosts",
+            width = 8.9,
+            girafeOutput("map")),
+        box(title = "The Most Common Language In Eurovision Songs",
+            width = 12,
+            girafeOutput("circles")),
+        box(
+          title = "Winners And Their Songs",
+          width = 12,
+          highchartOutput("winners")
+        ),
+      )),
+      fluidRow(splitLayout(valueBoxOutput("contests")))
+    ),
+    tabItem(
+      tabName = "other",
+      includeCSS("www/style.css"),
+      fluidRow(column(
         width = 10,
-        girafeOutput("circles")
-    ))
-  )
+        div(
+          style = "display: flex; align-items: baseline; justify-content: left; margin-top: 1px;",
+          div("EUROVISION", style = "color: #FFF400; font-size: 30px; font-weight: 700; font-family: 'Cantarell', sans-serif; margin-right: 10px;"),
+          div("STATISTICS", style = "color: white; font-size: 30px; font-weight: 400; font-family: 'Cantarell', sans-serif;")
+        )
+      )),
+      fluidRow(splitLayout(
+        box(
+          selectInput(
+            "select",
+            "Select a time-period:",
+            choices = list("1975 - 2003", "2004 - 2015", "2016 - 2024"),
+            selected = c(1975, 2003)
+          ),
+          height = 170,
+        ),
+        box(title = "Most Points Scored",
+            width = 12,
+            girafeOutput("points")),
+      ))
+    )
+  ))
 )
-)
+  
