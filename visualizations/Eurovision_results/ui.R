d130aa05dbbbb81d9a1859c315cc36992808e1a5
marcelrojo <rojewskimarcel@gmail.com>
2024-06-08 17:21:44 +0200

    Update visualizations folder

--- visualizations/Eurovision_results/ui.R
+++ visualizations/Eurovision_results/ui.R
@@ -1,13 +1,21 @@
 library(shiny)
 library(ggiraph)
 
+data_UI <- read.csv("datasets/eurovision_results.csv")
+countries<-sort(unique(data_UI$Country))
+years<-sort(unique(data_UI$Year))
+places<-sort(as.numeric(unique(data_UI$Grand.Final.Place)))
+
 ui <- fluidPage(
   titlePanel("Eurovision Results"),
   sidebarLayout(
     sidebarPanel(
-      selectInput("countries", "Select up to 5 countries:", choices = unique(data$Country), multiple = TRUE),
-      sliderInput("years", "Select Year Range:", min = min(data$Year), max = max(data$Year),
-                  value = c(min(data$Year), max(data$Year)), step = 1, sep = "")
+      selectInput("countries", "Select Countries:", choices = countries,
+                  selected="Poland", multiple = TRUE),
+      sliderInput("years", "Select Year Range:", min = min(years), max = max(years),
+                  value = c(min(years), max(years)), step = 1, sep = ""),
+      sliderInput("places", "Select Place Range:", min = min(places), max = max(places),
+                  value = c(min(places), max(places)), step = 1, sep = "")
     ),
     mainPanel(
       girafeOutput("eurovision_plot")
